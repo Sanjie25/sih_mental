75e8a8fde9c15358effa0c3166441925aead6142
Sanjie25 <sanjeev3re@gmail.com>
2025-09-09 21:00:31 +0530

    just a commit of functions

--- api/routes/bookings.py
+++ api/routes/bookings.py
@@ -12,18 +12,18 @@ from marshmallow import ValidationError
 
 bookings = Blueprint("bookings", __name__)
 
-
-@bookings.route("/bookings", methods=["POST"])
-@login_required
-def bookings():
-    bookings = db.session.execute(
-        db.select(Appointments).filter_by(customer_id=current_user.id)
-    ).fetchall()
-    result = {}
-    n = 0
-
-    for booking in bookings:
-        n += 1
-        result[n] = appointments_schema.dump(booking[0])
-
-    return success_response(result)
+#
+# @bookings.route("/bookings", methods=["POST"])
+# @login_required
+# def bookings():
+#     bookings = db.session.execute(
+#         db.select(Appointments).filter_by(customer_id=current_user.id)
+#     ).fetchall()
+#     result = {}
+#     n = 0
+#
+#     for booking in bookings:
+#         n += 1
+#         result[n] = appointments_schema.dump(booking[0])
+#
+#     return success_response(result)
